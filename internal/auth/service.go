af111286853a9be2e32c9889c9366c6153f4d790
怪哉GuaiZai <guaizai233@yeah.net>
2026-03-18 17:45:37 +0800

    feat(auth): enhance user registration with username validation

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -3,19 +3,22 @@ package auth
 import (
 	"errors"
 	"github.com/GuaiZai233/Larvar/internal/db"
+	"regexp"
 )
+
+var usernameRegexp = regexp.MustCompile(`^[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*$`)
 
 func LoginUser(username string, password string) (string, error) {
 	// get DB
 	database := db.GetDB()
 
 	// query if user exists
-	user, err := IsUserExist(database, username)
+	user, err := FindUserByAccount(database, username)
 	if err != nil {
 		return "", err
 	}
 	if user == nil {
-		return "", errors.New("用户名或密码错误") // 用户不存在
+		return "", errors.New("用户名/邮箱或密码错误") // 用户不存在
 	}
 
 	// verify password
@@ -34,6 +37,9 @@ func LoginUser(username string, password string) (string, error) {
 }
 
 func RegisterUser(username, password, email string) error {
+	if !usernameRegexp.MatchString(username) {
+		return errors.New("用户名只能包含字母、数字和连字符，且不能以连字符开头或结尾")
+	}
 	// get DB handle
 	database := db.GetDB()
 
